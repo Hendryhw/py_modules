b6f98f673a8ebab3605fb2e8a50097f45f0c7976
Bo-cheng Jan <school510587@yahoo.com.tw>
2016-05-14 23:33:06 +0800

    file_handles.py: Remove trailing whitespaces

--- file_handles.py
+++ file_handles.py
@@ -12,7 +12,7 @@ def get_directory_full_list(path):
          full_path_dir = os.path.join(dirpath, dir)
          full_directories.append(full_path_dir)
          directory_numbers += 1
-   
+
    print("--------------- end")
    print("Total directory # : " + str(directory_numbers))
    return full_directories
@@ -36,14 +36,14 @@ def get_file_full_list(path):
    print("--------------- end")
    print("Total file # : " + str(file_numbers))
    return full_files
-   
+
 def read_file(path):
    file_content = ""
    with open(path, 'r', encoding = reading_encoding) as file:
       file_content = file_content + file.read()
    file.close()
    return file_content
-   
+
 class file_handles_test(unittest.TestCase):
    def test_get_directory_full_list(self):
       root = r"..\ruten"
